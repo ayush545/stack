1b5f4cd191aecadcb8634dcf7f90c1099108b922
ayush545 <ayush.sedhai@deerwalk.edu.np>
2016-06-26 11:06:55 +0000

    Add files via upload

---
+++ assignment2.cpp
@@ -0,0 +1,70 @@
+
+
+//Write a program to implement stack which should be able to check the condition of underflow and overflow.
+
+# include <stdio.h>
+# define size 5
+
+struct stack
+{
+	int items[size];
+	int top;
+}s;
+
+int main()
+{
+	int choice, element;
+	s.top = 0;
+	retry:
+	printf("1. Enter element in Stack.\n2. Pull element from Stack.\n3. Exit\n");
+	printf("Enter your choice : ");
+	scanf("%d", &choice);
+	switch (choice)
+	{
+		case 1:
+			if (s.top == 5)
+			{
+				printf("Error Overflow\n\n");
+			}
+			else
+			{
+				printf("Enter the element to enter : ");
+				scanf("%d", &element);
+				push(element);
+			}
+			break;
+		case 2:
+			if (s.top == 0)
+			{
+				printf("Error Underflow\n\n");
+			}
+			else
+			{
+				element=pop();
+				printf("Element pulled from Stack : %d", element);
+			}
+			break;
+		case 3:
+			goto exit;
+			break;
+		default:
+			printf("Error. Try Again\n\n");
+			break;
+	}
+	goto retry;
+	exit:
+	return 0;
+}
+
+void push(int value) 
+{
+   s.items[s.top] = value;
+   s.top++;
+}
+ 
+int pop() 
+{
+   s.top--;
+   return s.items[s.top];
+}   
+
